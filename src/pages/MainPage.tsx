3f4cf3db7acc84bfa0cca807634f7344101789ae
Elpah <obengelpachris@gmail.com>
2026-02-17 04:35:23 +0100

    created main page

---
+++ src/pages/MainPage.tsx
@@ -0,0 +1,9 @@
+const MainPage = () => {
+  // @return
+  return (
+    <main className="min-h-screen bg-slate-950 text-slate-200 font-sans selection:bg-amber-500/30 selection:text-amber-200">
+    </main>
+  );
+};
+
+export default MainPage;
